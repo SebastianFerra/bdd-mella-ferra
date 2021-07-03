c4daa97636b86161f9dc5639e04a787d069cf6cb
sebadht <scasasu@uc.cl>
2021-07-02 23:25:21 -0400

    migracion usuarios casi completa

--- Sites/manejo_dbs/usuarios.php
+++ Sites/manejo_dbs/usuarios.php
@@ -22,33 +22,73 @@
 	$result3 -> execute();
 	$direcciones_personal = $result3 -> fetchAll();
 
+  $query4 = "SELECT usuario, direccion FROM direcciones_usuario";
+  $result4 = $db2 -> prepare($query4);
+	$result4 -> execute();
+	$direcciones_usuario = $result4 -> fetchAll();
+
+  $query5 = "SELECT * FROM direcciones_usuario";
+  $result5 = $db2 -> prepare($query5);
+	$result5 -> execute();
+	$direcciones_usuario_full = $result5 -> fetchAll();
+
   $count = 1;
-    foreach ($personal_admin as $p) {
-
-        if (false == in_array($p, $usuarios)) {
-          $p[0] = 364 + $count;
-          array_push($usuarios, $p);
-          $count = $count + 1;
-        }
-
+  foreach ($personal_admin as $p) {
+    foreach($direcciones_personal as $d) {
+      if ($d[0] == $p[0]) {
+        $d[0] = 364 + $count;
+      } 
     }
+    $count = $count + 1;
+  }
+
+  $user_count = 1;
+    foreach ($personal_admin as $p) {
+      if (false == in_array($p, $usuarios)) {
+        $p[0] = 364 + $user_count;
+        array_push($usuarios, $p);
+        $user_count = $user_count + 1;
+      }
+    }
+  
+  $dir_count = 1;
+  foreach ($direcciones_personal as $d) {
+    if (false == in_array($d, $direcciones_usuario)) {
+      $new_d = array(938 + $dir_count, $d[0], $d[1]);
+      array_push($direcciones_usuario_full, $new_d);
+      $dir_count = $dir_count + 1;
+    }
+  }
   ?>
 
-  <table>
-    <tr>
-      <th>id</th>
-      <th>nombre</th>
-      <th>rut</th>
-      <th>edad</th>
-      <th>sexo</th>
-    </tr>
-  
+  <div>
+    <table style="float: left">
+      <tr>
+        <th>id</th>
+        <th>nombre</th>
+        <th>rut</th>
+        <th>edad</th>
+        <th>sexo</th>
+      </tr>
       <?php
         foreach ($usuarios as $u) {
           echo "<tr><td>$u[0]</td><td>$u[1]</td><td>$u[2]</td><td>$u[3]</td><td>$u[4]</td></tr>";
       }
       ?>
+    </table>
+    <table style="float: left">
+      <tr>
+        <th>id usario direccion</th>
+        <th>id usuario</th>
+        <th>id direccion</th>
+      </tr>
+      <?php
+        foreach ($direcciones_usuario_full as $u) {
+          echo "<tr><td>$u[0]</td><td>$u[1]</td><td>$u[2]</td><td>$u[3]</td><td>$u[4]</td></tr>";
+      }
+      ?>
       
-  </table>
+    </table>
+  </div>
 
 <?php include('../templates/footer.html'); ?>
