881e4d464c595b77009c6cad7daff030271012d2
sebadht <scasasu@uc.cl>
2021-07-03 11:05:46 -0400

    migracion datos personal admin cer 1.5

--- Sites/manejo_dbs/usuarios.php
+++ Sites/manejo_dbs/usuarios.php
@@ -17,7 +17,7 @@
 	$result2 -> execute();
 	$usuarios = $result2 -> fetchAll();
 
-  $query3 = "SELECT personal_admin.id_persona, unidades.dirección FROM personal_admin, unidades WHERE personal_admin.uid = unidades.uid";
+  $query3 = "SELECT personal_admin.id_persona, unidades.dirección FROM personal_admin, unidades WHERE personal_admin.unidad = unidades.id";
   $result3 = $db1 -> prepare($query3);
 	$result3 -> execute();
 	$direcciones_personal = $result3 -> fetchAll();
