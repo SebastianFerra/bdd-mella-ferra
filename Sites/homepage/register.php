79ea9e1c62a7f9ab00045ea17d3b096fb1266a94
sebadht <scasasu@uc.cl>
2021-07-04 20:55:00 -0400

    Implementacion migracion de usuarios y generacion de contraseñas, nuevo homepage con login y register, falta validacion de usuarios

---
+++ Sites/homepage/register.php
@@ -0,0 +1,24 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title> Tienda Onlain TuShop </title>
+    <!-- Bootstrap(CSS), Jquery (javascripts), etc... -->
+
+    <link href ="styles/mystyles.css" rel="stylesheet" />
+
+</head>
+
+<div id="datos_usuario_nuevo">
+    <h2>INGRESA TUS DATOS PARA CREAR TU CUENTA:)</h2>
+    <label for="nombre"><h3>Nombre:</h3></label>
+    <input type="text" id="nombre"> 
+    <label for="rut"><h3>RUT:</h3></label>
+    <input type="text" id="rut"> 
+    <label for="edad"><h3>Edad:</h3></label>
+    <input type="text" id="edad"> 
+    <label for="direccion"><h3>Dirección:</h3></label>
+    <input type="text" id="direccion"> 
+    <br>
+    <br>
+    <br>
+    <button class="boton2">ACEPTAR</button>
+</div>
--- Sites/index.php
+++ Sites/index.php
@@ -1,134 +1,58 @@
-<?php include('templates/header.html');   ?>
+<!DOCTYPE html>
 
-<body>
-  <h1 align="center">Empresa de despachos </h1>
-  <p style="text-align:center;">Aquí podrás encontrar información sobre los despachos de tu empresa favorita</p>
+<html lang="es">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title> Tienda Onlain TuShop </title>
+    <!-- Bootstrap(CSS), Jquery (javascripts), etc... -->
 
-  <br>
+    <link href ="styles/mystyles.css" rel="stylesheet" />
+  
+  </head>
+  <body>
+    <!-- Migracion de personal admin y generacion de passwords -->
+    <?php include('manejo_dbs/usuarios.php');?>
+    <?php include('manejo_dbs/pass_gen.php');?>
 
-  <h3 align="center"> Listado con las direcciones de nuestras unidades de despacho</h3>
 
-  <form align="center" action="manejo_dbs/usuarios.php" method="post">
+    <div id="inicio">
+      <div id="titulo_portada">
+        <h1>MARISTAZO UNIMARC</h1>
+      </div>
+      <div id="botones1">
+        <a href="homepage/login.php">
+          <div class="boton1">INICIAR SESION</div>
+        </a>
+        <div class="espaciador1"></div>
+        <a href="homepage/register.php">
+          <div class="boton1">REGISTRATE</div>
+        </a>
+      </div>
+    </div>
+
+    <?php
+      $query_test = 'SELECT * FROM usuarios';
+      $test_result = $db2 -> prepare($query_test);
+      $test_result -> execute();
+      $usuarios = $test_result -> fetchAll();
+    ?>
     
-    <br/><br/>
-    <input type="submit" value="Mostrar todo">
-  </form>
+  <table>
+    <tr>
+      <th>id</th>
+      <th>nombre</th>
+      <th>rut</th>
+      <th>edad</th>
+      <th>sexo</th>
+      <th>pass</th>
+    </tr>
   
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center"> Ingrese una comuna para ver los vehículos de las unidades que se ubican allí</h3>
-
-  <form align="center" action="consultas/consulta2.php" method="post">
-    Comuna:
-    <input type="text" name="comuna">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center"> Ingrese una comuna y seleccione un año para ver todos los vehículos que realizaron despachos allí en ese año</h3>
-  <form align="center" action="consultas/consulta3.php" method="post">
-    Comuna:
-    <input type="text" name="altura">
-    <br/><br/>
-    
-  
-  <?php
-  #	AQUI OBTENEMOS LAS COSAS QUE NECESITAMOS COMO LOS AÑOS Y ESAS COSAS
-  require("config/conexion.php");
-  $result = $db1 -> prepare("SELECT DISTINCT DATE_PART('year',fecha) AS año FROM despachos ORDER BY año;");
-  $result -> execute();
-  $dataCollected = $result -> fetchAll();
-  ?>
-    Seleccinar una año:
-    <select name="año">
       <?php
-      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-      foreach ($dataCollected as $d) {
-        echo "<option value=$d[0]>$d[0]</option>";
+        foreach ($usuarios as $p) {
+          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td></tr>";
       }
       ?>
-    </select>
-	<br/><br/>
-	<input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center"> Ingrese un tipo de vehículo y 2 números para ver qué despachos fueron realizados</h3>
-  <h3>por repartidores cuya edad está en el rango y por medio de ese tipo de vehículo</h3>
-  <form align="center" action="consultas/consulta4.php" method="post">
-    Tipo de vehículo:
-    <input type="text" name="vehículo">
-    <br/><br/>
-
-  
-  <?php
-  #	AQUI OBTENEMOS LAS COSAS QUE NECESITAMOS COMO LOS AÑOS Y ESAS COSAS
-  require("config/conexion.php");
-  $result = $db1 -> prepare("SELECT DISTINCT edad FROM personal ORDER BY edad;");
-  $result -> execute();
-  $dataCollected = $result -> fetchAll();
-  ?>
-    Seleccinar año inicio:
-    <select name="inicio">
-      <?php
-      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-      foreach ($dataCollected as $d) {
-        echo "<option value=$d[0]>$d[0]</option>";
-      }
-      ?>
-    </select>
-	<br><br>
-	Seleccionar año término:
-	<select name="termino">
-      <?php
-      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-      foreach ($dataCollected as $d) {
-        echo "<option value=$d[0]>$d[0]</option>";
-      }
-      ?>
-    </select>
-	<br/><br/>
-	<input type="submit" value="Buscar">
-  </form>
-
-  <br>
-  <br>
-  <br>
-  <h3 align="center"> Ingrese 2 comunas y vea los nombres de los jefes de las unidades que realizaron despachos a ambas comunas </h3>
-
-  <form align="center" action="consultas/consulta5.php" method="post">
-    Comuna 1:
-    <input type="text" name="comuna1">
-    <br/><br/>
-	Comuna 2:
-	<input type="text" name="comuna2">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
-  <h3 align="center"> Ingrese un tipo de vehículo para saber qué unidad maneja más vehículos de tal tipo </h3>
-
-  <form align="center" action="consultas/consulta6.php" method="post">
-   Tipo de vehículo:
-    <input type="text" name="tipo">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
-  <div>
-            <img src="us1.JPG" alt="us" />
-        </div>
-</body>
+  </table>
+  </body>
 </html>
+
