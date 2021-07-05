ea00530c0bc8f15507d8728137fbdf510566d6b8
sebadht <scasasu@uc.cl>
2021-07-05 16:42:25 -0400

    asdfa

--- Sites/manejo_dbs/validacion_register.php
+++ Sites/manejo_dbs/validacion_register.php
@@ -11,10 +11,16 @@
   $users = $result -> fetchAll();
 
   $id_user = count($users);  
-  $name = $_POST["nombre"];
+  $nombre = $_POST["nombre"];
   $rut = $_POST["rut"];
-  $age = $_POST["edad"];
-  $addres = $_POST["direccion"];
+  $edad = $_POST["edad"];
+  $direccion = $_POST["direccion"];
+  $comuna = $_POST["comuna"];
+
+  $query_addres = "SELECT * FROM direcciones WHERE nombre_direccion = '$direccion' AND comuna = '$comuna'";
+  $result_addres = $db2 -> prepare($query_addres);
+  $result_addres -> execute();
+  $addres = $result_addres -> fetchAll();
 
   $rut_exists = false;
   foreach ($users as $u) {
@@ -26,7 +32,10 @@
   if ($rut_exists) {
     echo "Lo sentimos pero ese rut ya existe"; 
   } elseif (false == $rut_exists) {
-    ;
+    if (count($addres) == 1) {
+      $id_direccion = $addres[0]['id_direccion'];
+    }
+
   }
 ?>
 <br>
