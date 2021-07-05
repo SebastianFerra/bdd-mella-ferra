f8099aa4ce2473b138368dd282c5c97275428f52
sebadht <scasasu@uc.cl>
2021-07-04 22:38:38 -0400

    jaklsdjfl;a

--- Sites/index.php
+++ Sites/index.php
@@ -11,8 +11,8 @@
   </head>
   <body>
     <!-- Migracion de personal admin y generacion de passwords -->
-    <?php include('manejo_dbs/usuarios.php');?>
-    <?php include('manejo_dbs/pass_gen.php');?>
+    <?php require('manejo_dbs/usuarios.php');?>
+    <?php require('manejo_dbs/pass_gen.php');?>
 
 
     <div id="inicio">
