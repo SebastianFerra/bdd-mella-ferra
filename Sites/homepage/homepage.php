6c16bca8aa19e1d6118e46a94a33af076146a8b1
sebadht <scasasu@uc.cl>
2021-07-06 14:22:26 -0400

    aaaaaaaaaaa

--- Sites/homepage/homepage.php
+++ Sites/homepage/homepage.php
@@ -9,35 +9,37 @@
 
 <?php
     $id_user = intval($_POST["id_user"]);
+    echo $id_user;
 
-    $query_user = "SELECT * FROM usuarios WHERE id_usuario = $id_user";
-    $result_user = $db2 -> prepare($query_user);
-    $result_user -> execute();
-    $user = $result_user -> fetchAll();
+    #$query_user = "SELECT * FROM usuarios WHERE id_usuario = $id_user";
+    #$result_user = $db2 -> prepare($query_user);
+    #$result_user -> execute();
+    #$user = $result_user -> fetchAll();
 
-    $query_dir = "SELECT direcciones.nombre_direccion, direcciones.comuna 
-        FROM direcciones, direcciones_usuario 
-        WHERE direcciones_usuario.usuario = $id_user 
-        AND direcciones.id_direccion = direcciones_usuario.direccion";
-    $result_dir = $db2 -> prepare($query_dir);
-    $result_dir = $db2 -> execute();
-    $direccion = $result_dir -> fetchAll();
+    #$query_dir = "SELECT direcciones.nombre_direccion, direcciones.comuna 
+    #    FROM direcciones, direcciones_usuario 
+    #    WHERE direcciones_usuario.usuario = $id_user 
+    #    AND direcciones.id_direccion = direcciones_usuario.direccion";
+    #$result_dir = $db2 -> prepare($query_dir);
+    #$result_dir = $db2 -> execute();
+    #$direccion = $result_dir -> fetchAll();
     
-    $direccion_data = $direccion[0];
-    $user_data = $user[0];
+    #$direccion_data = $direccion[0];
+    #$user_data = $user[0];
 ?>
 
+<!--
 <div id="datos_perfil">
     <h2>HOLA USUARIO :)</h2>
     <h3>INFORMACIÓN PERSONAL</h3>
     <ul>
         <?php
-        echo '
-        <li>Nombre: $user_data["nombre"]</li>
-        <li>Edad: $user_data["edad"]</li>
-        <li>RUT: $user_data["rut"]</li>
-        <li>Dirección: $direccion_data["nombre_direccion"], $direccion_data["comuna"]</li>
-        '
+        #echo '
+        #<li>Nombre: $user_data["nombre"]</li>
+        #<li>Edad: $user_data["edad"]</li>
+        #<li>RUT: $user_data["rut"]</li>
+        #<li>Dirección: $direccion_data["nombre_direccion"], $direccion_data["comuna"]</li>
+        #'
         ?>
     </ul>
     <a>
@@ -49,3 +51,4 @@
     </a>
 
 </div>
+-->
