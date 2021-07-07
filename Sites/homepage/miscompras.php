fa47a8b28c0cc7bc84ce51d961f45fff25489806
sebaferra123 <sferra@uc.cl>
2021-07-07 02:20:33 -0400

    patito lindo

---
+++ Sites/homepage/miscompras.php
@@ -0,0 +1,38 @@
+head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title> Tienda Onlain TuShop </title>
+    <!-- Bootstrap(CSS), Jquery (javascripts), etc... -->
+
+    <link href ="../styles/style.css" rel="stylesheet" />
+
+</head>
+
+<?php
+    require("../config/conexion.php");
+
+    $id_user = $_POST["id_user"];
+   
+    $query = "SELECT Compras.producto, Compras.cantidad
+    FROM Compras, Carritos, Usuarios
+    WHERE id_usuario = Carritos.comprador
+    AND Carritos.id = Compras.carrito
+    ";
+    $result = $db2 -> prepare($query);
+    $result -> execute();
+    $compras = $result -> fetchAll();
+    
+?>
+<table>
+    <tr>
+      <th>Historial de compras</th>
+    </tr>
+  
+      <?php
+        // echo $pokemones;
+        foreach ($compras as $p) {
+          echo "<tr><td>$p[0]</td></tr>";
+      }
+      ?>
+      
+  </table>
+
